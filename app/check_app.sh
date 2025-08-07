43ee6f039bcfca9422c3789bb833b6535bf78112
Safia Meradji <safia.meradji@laplateforme.io>
2025-08-07 10:13:46 +0200

    Simule un échec pour analyse des logs

--- app/check_app.sh
+++ app/check_app.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "Début des vérifications..."
-if [ -f "./app/index.html" ]; then
-    echo "✔️ Fichier index.html présent"
-    exit 0
-else
-    echo "❌ Fichier index.html manquant"
-    exit 1
-fi
+echo "❌ Fichier index.html manquant"
+exit 1
